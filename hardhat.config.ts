ab7687a73ea2ac2c03d540551c31e007b8c9f955
Odi <muhammad.mustadi@gmail.com>
2024-02-10 23:46:18 +0700

    Refactor: Use foundry strucure and add tests (#2)
    * chore: forge init
    
    * forge install: forge-std
    
    v1.7.6
    
    * refactor: use foundry arch and add initial tests
    
    * add remappings.txt
    
    * rename test contract, remove scripts
    
    * add hardhat-foundry
    
    * unfold contracts directory

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-toolbox"
 import "hardhat-gas-reporter"
 
---
+++ test/ERC404.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/examples/ExampleERC404.sol";
+
+contract Erc404Test is Test {
+    ExampleERC404 public simpleContract_;
+
+    string name_ = "Example";
+    string symbol_ = "EXM";
+    uint8 decimals_ = 18;
+    uint256 maxTotalSupplyNft_ = 100;
+
+    address initialOwner_ = address(0x1);
+    address initialMintRecipient_ = address(0x2);
+
+    function setUp() public {
+        simpleContract_ =
+            new ExampleERC404(name_, symbol_, decimals_, maxTotalSupplyNft_, initialOwner_, initialMintRecipient_);
+    }
+
+    function test_initializeSimple() public {
+        assertEq(simpleContract_.name(), name_);
+        assertEq(simpleContract_.symbol(), symbol_);
+        assertEq(simpleContract_.decimals(), decimals_);
+        assertEq(simpleContract_.owner(), initialOwner_);
+
+        // initial balance is 100 ** decimals ERC20, but 0 NFT
+        // ExampleERC404.sol:L18
+        assertEq(simpleContract_.balanceOf(initialMintRecipient_), maxTotalSupplyNft_ * 10 ** decimals_);
+        assertEq(simpleContract_.owned(initialMintRecipient_).length, 0);
+    }
+}
