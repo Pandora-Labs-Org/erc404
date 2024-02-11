c453cc57c35877ad4c2f31f54c7daeb418dc7afb
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2024-02-11 22:22:37 +0700

    test: deployMinimalERC404 and deployExampleERC404WithSomeTokensTransferredToRandomAddress

--- test/ERC404.t.sol
+++ test/ERC404.t.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/examples/ExampleERC404.sol";
-import "../src/interfaces/IERC404.sol";
+import "../src/mocks/MinimalERC404.sol";
+import {IERC404} from "../src/interfaces/IERC404.sol";
+import "../src/ERC404.sol";
 
+// deployExampleERC404
+// deployExampleERC404WithSomeTokensTransferredToRandomAddress
 contract Erc404Test is Test {
     ExampleERC404 public simpleContract_;
 
@@ -17,6 +21,7 @@ contract Erc404Test is Test {
     address initialOwner_ = address(0x1);
     address initialMintRecipient_ = address(0x2);
 
+    ///////////////////////////// deployMinimalERC404()
     function setUp() public {
         simpleContract_ =
             new ExampleERC404(name_, symbol_, decimals_, maxTotalSupplyNft_, initialOwner_, initialMintRecipient_);
@@ -46,13 +51,14 @@ contract Erc404Test is Test {
         assertTrue(simpleContract_.whitelist(initialMintRecipient_));
     }
 
-    function test_tokenTransfer(uint8 nftToTransfer) public {
+    ///////////////////////////// deployExampleERC404WithSomeTokensTransferredToRandomAddress()
+    function test_tokenTransfer(uint8 nftToTransfer, address randomAddress) public {
         vm.assume(nftToTransfer <= 100);
-        address alice = address(0xa);
+        vm.assume(randomAddress != address(0));
 
         // Transfer some tokens to a non-whitelisted wallet to generate the NFTs.
         vm.prank(initialMintRecipient_);
-        simpleContract_.transfer(alice, nftToTransfer * units_);
+        simpleContract_.transfer(randomAddress, nftToTransfer * units_);
 
         // Returns the correct total supply
         assertEq(simpleContract_.erc721TotalSupply(), nftToTransfer);
@@ -68,7 +74,97 @@ contract Erc404Test is Test {
         simpleContract_.ownerOf(nftToTransfer + 1);
 
         for (uint8 i = 1; i <= nftToTransfer; i++) {
-            assertEq(simpleContract_.ownerOf(i), alice);
+            assertEq(simpleContract_.ownerOf(i), randomAddress);
         }
+    }
+}
+
+// deployMinimalERC404
+contract Erc404MinimalTest is Test {
+    MinimalERC404 public minimalContract_;
+
+    string name_ = "Example";
+    string symbol_ = "EXM";
+    uint8 decimals_ = 18;
+    uint256 units_ = 10 ** decimals_;
+    uint256 maxTotalSupplyNft_ = 100;
+    uint256 maxTotalSupplyCoin_ = maxTotalSupplyNft_ * units_;
+
+    address initialOwner_ = address(0x1);
+
+    event ERC721Transfer(address indexed from, address indexed to, uint256 indexed id);
+    event ERC20Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function setUp() public {
+        minimalContract_ = new MinimalERC404(name_, symbol_, decimals_, initialOwner_);
+    }
+
+    function test_initializeMinimal() public {
+        assertEq(minimalContract_.name(), name_);
+        assertEq(minimalContract_.symbol(), symbol_);
+        assertEq(minimalContract_.decimals(), decimals_);
+        assertEq(minimalContract_.owner(), initialOwner_);
+    }
+
+    function test_mintFullSupply_20_721(address recipient) public {
+        vm.assume(recipient != address(0));
+
+        // Owner mints the full supply of ERC20 tokens (with the corresponding ERC721 tokens minted as well)
+        vm.prank(initialOwner_);
+        minimalContract_.mintERC20(recipient, maxTotalSupplyCoin_, true);
+
+        // Expect the total supply to be equal to the max total supply
+        assertEq(minimalContract_.totalSupply(), maxTotalSupplyCoin_);
+        assertEq(minimalContract_.erc20TotalSupply(), maxTotalSupplyCoin_);
+
+        // Expect the minted count to be equal to the max total supply
+        assertEq(minimalContract_.erc721TotalSupply(), maxTotalSupplyNft_);
+    }
+
+    function test_mintFullSupply_20(address recipient) public {
+        vm.assume(recipient != address(0));
+
+        // Owner mints the full supply of ERC20 tokens only
+        vm.prank(initialOwner_);
+        minimalContract_.mintERC20(recipient, maxTotalSupplyCoin_, false);
+
+        // Expect the total supply to be equal to the max total supply
+        assertEq(minimalContract_.totalSupply(), maxTotalSupplyCoin_);
+        assertEq(minimalContract_.erc20TotalSupply(), maxTotalSupplyCoin_);
+
+        // Expect the minted count to be equal to 0
+        assertEq(minimalContract_.erc721TotalSupply(), 0);
+    }
+
+    function test_erc721Storage(uint8 nftQty, address recipient) public {
+        vm.assume(nftQty < maxTotalSupplyNft_);
+        vm.assume(recipient != address(0) && recipient != initialOwner_);
+
+        // Total supply should be 0
+        assertEq(minimalContract_.erc721TotalSupply(), 0);
+
+        // Expect the contract's bank to be empty
+        assertEq(minimalContract_.balanceOf(address(minimalContract_)), 0);
+        assertEq(minimalContract_.erc721TokensBankedInQueue(), 0);
+
+        uint256 value = nftQty * units_;
+
+        // mint at the bottom, setup expected events first
+
+        // expect 1 erc20 transfer event
+        // Check for ERC20Transfer mint events (from 0x0 to the recipient)
+        vm.expectEmit(false, false, false, true);
+        emit ERC20Transfer(address(0), recipient, value);
+
+        // expect multiple erc721 transfers
+        for (uint8 i = 1; i <= nftQty; i++) {
+            // Check for ERC721Transfer mint events (from 0x0 to the recipient)
+            vm.expectEmit(true, true, true, true);
+            emit ERC721Transfer(address(0), recipient, i);
+        }
+
+        // mint as owner
+        vm.prank(initialOwner_);
+        minimalContract_.mintERC20(recipient, value, true);
     }
 }
