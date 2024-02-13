a61248002e4d61bf657f3da01a507d8576f4815d
caldereth <135465775+caldereth@users.noreply.github.com>
2024-02-13 15:59:40 +1300

    tests for invalid/valid erc721receiver contract recipients

---
+++ contracts/mocks/MockInvalidERC721Receiver.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MockInvalidERC721Receiver {
+ 
+
+   function wrongSelector(
+  ) external pure {
+    // NOOP
+  }
+
+  function onERC721Received(
+    address,
+    address,
+    uint256,
+    bytes calldata
+  ) external pure returns (bytes4) {
+    return MockInvalidERC721Receiver.wrongSelector.selector;
+  }
+}
